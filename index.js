342150ff0e3da6185164fa5037b33eb1e48442ac
noob-ubaid <ubaidurrahman661@gmail.com>
2025-09-06 20:10:59 +0600

    added post method for logged in user

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config();
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
 const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@ubaid-database.njfi7n5.mongodb.net/?retryWrites=true&w=majority&appName=Ubaid-Database`;
-
 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 async function run() {
   try {
-  
+    const dataBase = client.db("Medimart");
+    const usersCollection = dataBase.collection("users");
+    // ? create user and store into db
+    app.post('/users', async (req,res)=>{
+        const data = req.body
+        const query = {email : data.email}
+        const alreadyExist = await usersCollection.findOne(query)
+        if(!!alreadyExist){
+            return res.send({message : 'User already exist'})
+        }
+        const result = await usersCollection.insertOne(data)
+        res.send(result)
+    })
   } finally {
-   
   }
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
